99e910af5a42e02a1ba8fef3f7fabccaf3260873
aircongo <natelufuluabo@yahoo.ca>
2025-04-22 20:34:55 -0400

    basic layout for reservation page done

---
+++ src/components/reservation/ReservationBox.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="reservations-box">
+    <div class="title-description-container">
+      <h1 class="box-title">{{ boxTitle }}</h1>
+      <p class="box-description">{{ boxDescription }}</p>
+    </div>
+    <p class="no-reservation-text">{{ noReservationText }}</p>
+  </div>
+</template>
+<script>
+export default {
+  name: 'ReservationBox',
+  props: {
+    boxTitle: { type: String, required: true },
+    boxDescription: { type: String, required: true },
+    noReservationText: { type: String, required: true },
+  },
+}
+</script>
+<style scoped>
+.reservations-box {
+  background-color: #0f172a;
+  border: 0.15rem solid #1a365d;
+  border-radius: 0.75rem;
+  padding: 3rem;
+  display: flex;
+  flex-direction: column;
+  gap: 2rem;
+  min-height: 20rem;
+}
+.no-reservation-text {
+  font-size: 1.3rem;
+  align-self: center;
+  color: #d2d2d2;
+}
+.box-title {
+  font-size: 2.5rem;
+  font-weight: bold;
+  color: #fff;
+}
+.box-title {
+  font-size: 1.8rem;
+}
+.box-description {
+  color: #f1f5f9;
+  font-size: 1.1rem;
+}
+</style>
--- src/pages/Reservation.vue
+++ src/pages/Reservation.vue
@@ -1,3 +1,81 @@
 <template>
-  <div class="reservation-page-container">Reservation</div>
+  <div class="reservation-page-container">
+    <div class="page-header-container">
+      <div class="title-description-container">
+        <h1 class="page-title">Réservations</h1>
+        <p class="page-description">Gérez les réservations de votre restaurant</p>
+      </div>
+      <button @click="" class="add-reservation-btn">Nouvelle réservation</button>
+    </div>
+    <div class="input-container">
+      <input id="min-covers" type="text" placeholder="Réchercher des réservations..." />
+    </div>
+    <ReservationBox
+      boxTitle="Réservations à venir"
+      boxDescription="Convives attendus aujourd'hui (0)"
+      noReservationText="Aucune réservation à venir pour aujourd'hui"
+    />
+    <ReservationBox
+      boxTitle="Réservations placées"
+      boxDescription="Clients actuellement dans votre restaurant (0)"
+      noReservationText="Aucun client n'est actuellement à table"
+    />
+  </div>
 </template>
+<script setup>
+import ReservationBox from '@/components/reservation/ReservationBox.vue'
+</script>
+<style scoped>
+.reservation-page-container {
+  background-color: #1e293b;
+  min-height: 100%;
+  display: flex;
+  flex-direction: column;
+  gap: 3rem;
+  padding: 3rem;
+}
+.page-header-container {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+.page-title,
+.box-title {
+  font-size: 2.5rem;
+  font-weight: bold;
+  color: #fff;
+}
+.box-title {
+  font-size: 1.8rem;
+}
+.page-description,
+.box-description {
+  color: #f1f5f9;
+  font-size: 1.1rem;
+}
+.add-reservation-btn {
+  border: none;
+  background-color: rgb(0, 74, 177);
+  color: #fff;
+  font-size: 1.6rem;
+  padding: 1rem 2rem;
+  cursor: pointer;
+  border-radius: 0.75rem;
+  letter-spacing: 0.05rem;
+}
+.input-container > input {
+  background-color: #1e293b;
+  padding: 1rem;
+  border: 0.1rem solid #1a365d;
+  border-radius: 0.75rem;
+  color: #f1f5f9;
+  font-size: 1.4rem;
+  min-width: 30rem;
+}
+.input-container > input:focus {
+  outline: 0.2rem solid #1a365d;
+}
+.input-container > input::placeholder {
+  color: #d2d2d2;
+}
+</style>
