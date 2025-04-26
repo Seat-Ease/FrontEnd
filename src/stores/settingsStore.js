9570b304bfb590c7c5ddce7b1a5e14e04b8d654b
aircongo <natelufuluabo@yahoo.ca>
2025-04-25 21:27:00 -0400

    settings store done

---
+++ src/stores/settingsStore.js
@@ -0,0 +1,63 @@
+import { ref } from 'vue'
+import { defineStore } from 'pinia'
+
+export const settingsStore = defineStore('settingsStore', () => {
+  const restaurantData = ref({
+    name: 'Demo Restaurant',
+    address: '1, Ave Demo',
+    city: 'Kinshasa',
+    postal_code: 'kin111',
+    website_link: 'www.demo-restaurant.com',
+  })
+
+  function getRestaurantData() {
+    return restaurantData.value
+  }
+
+  function editRestaurantData(p_data) {
+    restaurantData.value = p_data
+  }
+
+  const scheduleData = ref({
+    sunday: 'off',
+    monday: '11:00 - 23:00',
+    tuesday: '11:00 - 23:00',
+    wednesday: '11:00 - 23:00',
+    thursday: '11:00 - 23:00',
+    friday: '11:00 - 23:00',
+    saturday: '11:00 - 23:00',
+  })
+
+  function getScheduleData() {
+    return scheduleData.value
+  }
+
+  function editScheduleData(p_data) {
+    scheduleData.value = p_data
+  }
+
+  const availabilitiesSettings = ref({
+    intervalle: '15',
+    est_srvice_duration: '90',
+  })
+
+  function getAvailabiltiesSettings() {
+    return availabilitiesSettings.value
+  }
+
+  function editAvailabiltiesSettings(p_data) {
+    availabilitiesSettings.value = p_data
+  }
+
+  return {
+    restaurantData,
+    getRestaurantData,
+    editRestaurantData,
+    scheduleData,
+    getScheduleData,
+    editScheduleData,
+    availabilitiesSettings,
+    getAvailabiltiesSettings,
+    editAvailabiltiesSettings,
+  }
+})
