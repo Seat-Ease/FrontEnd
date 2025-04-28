ae2e8b799b56aa387e6eae138b40981e0840c175
aircongo <natelufuluabo@yahoo.ca>
2025-04-28 19:01:11 -0400

    general info form done

---
+++ src/components/signup/GeneralInfo.vue
@@ -0,0 +1,114 @@
+<template>
+  <div class="general-info-container">
+    <div class="header-container">
+      <div class="title-description-container">
+        <p class="title">Informations générales</p>
+        <p class="description">Entrez les informations générales de votre restaurant.</p>
+      </div>
+    </div>
+    <form>
+      <div class="input-container">
+        <input v-model="generalInfo.name" id="restaurant-name" type="text" placeholder="Nom*" />
+      </div>
+      <div class="input-container">
+        <input
+          v-model="generalInfo.phone"
+          id="restaurant-phone"
+          type="number"
+          placeholder="Téléphone*"
+        />
+      </div>
+      <div class="input-container">
+        <input
+          v-model="generalInfo.address"
+          id="restaurant-address"
+          type="text"
+          placeholder="Adresse*"
+        />
+      </div>
+      <div class="input-container">
+        <input v-model="generalInfo.city" id="restaurant-city" type="text" placeholder="Ville*" />
+      </div>
+      <div class="input-container">
+        <input
+          v-model="generalInfo.postal_code"
+          id="restaurant-postal"
+          type="text"
+          placeholder="Code postal*"
+        />
+      </div>
+      <div class="input-container">
+        <input
+          v-model="generalInfo.website"
+          id="restaurant-website"
+          type="text"
+          placeholder="Site Web"
+        />
+      </div>
+    </form>
+  </div>
+</template>
+<script setup>
+import { ref } from 'vue'
+
+const generalInfo = ref({
+  name: '',
+  phone: '',
+  address: '',
+  city: '',
+  postal_code: '',
+  website: '',
+})
+</script>
+<style scoped>
+.general-info-container {
+  display: flex;
+  flex-direction: column;
+  gap: 2rem;
+  background-color: #0f172a;
+  padding: 3rem;
+  border: 0.1rem solid #1a365d;
+  border-radius: 0.75rem;
+}
+.header-container {
+  display: flex;
+  align-self: center;
+  justify-content: space-between;
+  width: 100%;
+}
+.title-description-container {
+  display: flex;
+  flex-direction: column;
+  gap: 0.75rem;
+}
+.title {
+  font-size: 1.6rem;
+  color: #fff;
+  font-weight: 600;
+}
+.description {
+  font-size: 1.2rem;
+  color: #f1f5f9;
+}
+form {
+  display: flex;
+  flex-direction: column;
+  gap: 2rem;
+  width: 100%;
+}
+.input-container > input {
+  background-color: #0f172a;
+  padding: 1rem;
+  border: 0.1rem solid #1a365d;
+  border-radius: 0.75rem;
+  color: #f1f5f9;
+  font-size: 1.4rem;
+  width: 100%;
+}
+.input-container > input:focus {
+  outline: 0.2rem solid #1a365d;
+}
+.input-container > input::placeholder {
+  color: rgb(161, 161, 161) 7;
+}
+</style>
--- src/pages/Signup.vue
+++ src/pages/Signup.vue
@@ -1,3 +1,51 @@
 <template>
-  <div class="sign-up-container">Sign up</div>
+  <div class="signup-container">
+    <div class="btns-container">
+      <button class="prev btn">Précedent</button>
+      <button class="next btn">Suivant</button>
+    </div>
+    <p class="redirect-login">
+      Déjà client ? Connectez-vous
+      <RouterLink to="/login">Ici</RouterLink>
+    </p>
+    <GeneralInfo />
+  </div>
 </template>
+<script setup>
+import GeneralInfo from '@/components/signup/GeneralInfo.vue'
+</script>
+<style scoped>
+.signup-container {
+  display: flex;
+  flex-direction: column;
+  gap: 2rem;
+  width: 40%;
+  margin: 5rem auto;
+}
+.btns-container {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+.btn {
+  border: none;
+  background-color: #1e293b;
+  color: #0d9488;
+  text-decoration: underline;
+}
+.redirect-login {
+  display: flex;
+  align-items: center;
+  gap: 0.5rem;
+  font-size: 1.2rem;
+  color: #f1f5f9;
+}
+.redirect-login a:link,
+.redirect-login a:visited,
+.redirect-login a:active,
+.redirect-login a:hover {
+  color: #0d9488;
+  font-weight: bold;
+  letter-spacing: 0.1rem;
+}
+</style>
