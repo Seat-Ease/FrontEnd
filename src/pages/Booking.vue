a7cea4847cf93b9868b922627feb67152c713c36
aircongo <natelufuluabo@yahoo.ca>
2025-06-16 19:56:09 -0400

    showing restaurant data on screen done

--- src/pages/Booking.vue
+++ src/pages/Booking.vue
@@ -1,8 +1,15 @@
 <template>
   <div v-if="pageFound === null" class="loading-container">Loading...</div>
-  <div v-else-if="pageFound === true">
-    <h1>Booking page</h1>
-    <div>Restaurant id: {{ $route.params.id }}</div>
+  <div v-else-if="pageFound === true" class="page-container">
+    <div class="restaurant-info-container">
+      <h3 class="name">{{ restaurantData?.general.name }}</h3>
+      <p class="location">
+        {{ restaurantData?.general.address }}, {{ restaurantData?.general.city }}
+      </p>
+      <p class="phone">
+        {{ phone_number_formatted }}
+      </p>
+    </div>
   </div>
   <div v-else class="not-found-container">
     <h1 class="oops-title">Oops!</h1>
@@ -18,10 +25,19 @@ import { bookingStore } from '@/stores/bookingStore'
 const route = useRoute()
 
 const pageFound = ref(null)
+const restaurantData = ref(null)
+
+const phone_number_formatted = ref(null)
 
 onMounted(async () => {
   try {
-    pageFound.value = await bookingStore().isIdValid(route.params.id)
+    if (await bookingStore().isIdValid(route.params.id)) {
+      pageFound.value = true
+      restaurantData.value = await bookingStore().getRestaurantData(route.params.id)
+      phone_number_formatted.value = `(${String(restaurantData.value.general.phone).slice(0, 3)}) ${String(restaurantData.value.general.phone).slice(3, 6)}-${String(restaurantData.value.general.phone).slice(6)}`
+    } else {
+      pageFound.value = false
+    }
   } catch (e) {
     console.log(e)
   }
@@ -37,6 +53,30 @@ onMounted(async () => {
   justify-content: center;
   font-size: 5rem;
   font-weight: bold;
+}
+.page-container {
+  display: flex;
+  flex-direction: column;
+  gap: 2rem;
+  background-color: #1e293b;
+  width: 100%;
+  padding: 0 2rem;
+  padding-top: 2rem;
+}
+.restaurant-info-container {
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem;
+}
+.name {
+  font-size: 1.6rem;
+  color: #fff;
+  letter-spacing: 0.1rem;
+}
+.location,
+.phone {
+  font-size: 1.2rem;
+  color: #cccccc;
 }
 .not-found-container {
   display: flex;
--- src/stores/bookingStore.js
+++ src/stores/bookingStore.js
@@ -4,12 +4,29 @@ import { getFirestore, doc, getDoc, updateDoc } from 'firebase/firestore'
 import { app } from '@/firebase'
 
 export const bookingStore = defineStore('bookingStore', () => {
-  async function isIdValid(id) {
-    const restaurantRef = doc(getFirestore(app), 'restaurants', id)
+  async function isIdValid(restaurant_id) {
+    const restaurantRef = doc(getFirestore(app), 'restaurants', restaurant_id)
     const snapshot = await getDoc(restaurantRef)
 
     return snapshot.exists()
   }
 
-  return { isIdValid }
+  async function getRestaurantData(restaurant_id) {
+    try {
+      const restaurantRef = doc(getFirestore(app), 'restaurants', restaurant_id)
+      const snapshot = await getDoc(restaurantRef)
+
+      if (!snapshot.exists()) {
+        console.warn('Aucun restaurant trouvé pour cet utilisateur.')
+        return null
+      }
+
+      return snapshot.data()
+    } catch (error) {
+      console.error('Erreur lors du chargement du restaurant :', error)
+      return null
+    }
+  }
+
+  return { isIdValid, getRestaurantData }
 })
